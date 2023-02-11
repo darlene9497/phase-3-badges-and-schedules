5273ab959be0495f9b3f1cab112750ac1fe9404a
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-02-11 20:41:31 +0300

    Complete Assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+    name.collect do |name|
+        badge_maker(name)
+    end
+end
+
+def assign_rooms(name)
+    name.collect.each_with_index do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+end
+
+def printer(name)
+    batch_badge_creator(name).each do |badge|
+        puts badge
+    end
+    assign_rooms(name).each do |assignment|
+        puts assignment
+    end
+end
+
+
+
+  
